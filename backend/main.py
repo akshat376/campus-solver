f54db076608554634f0e8df02753fcde41d439e6
akshat376 <akshat237pal@gmail.com>
2026-04-13 03:27:08 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -20,7 +20,7 @@ app.mount("/images", StaticFiles(directory=UPLOAD_DIR), name="images")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["http://localhost:5173", "https://campus-solver-upzz.vercel.app/"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
